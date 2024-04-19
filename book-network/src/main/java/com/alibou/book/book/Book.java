de461f00e0140b27ba9517523cdd119ff837072c
Nibras Sameeh <nibras.s@aeturnum.com>
2024-04-19 20:11:09 +0530

    added entities

---
+++ book-network/src/main/java/com/alibou/book/book/Book.java
@@ -0,0 +1,44 @@
+package com.alibou.book.book;
+
+import com.alibou.book.common.BaseEntity;
+import com.alibou.book.feedback.Feedback;
+import com.alibou.book.history.BookTransactionHistory;
+import com.alibou.book.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> bookTransactionHistories;
+}
--- book-network/src/main/java/com/alibou/book/user/User.java
+++ book-network/src/main/java/com/alibou/book/user/User.java
@@ -1,5 +1,7 @@
 package com.alibou.book.user;
 
+import com.alibou.book.book.Book;
+import com.alibou.book.history.BookTransactionHistory;
 import com.alibou.book.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
@@ -42,6 +44,12 @@ public class User implements UserDetails, Principal {
 
     @ManyToMany(fetch = FetchType.EAGER)
     private List<Role> roles;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    @OneToMany(mappedBy = "user")
+    private List<BookTransactionHistory> bookTransactionHistories;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
