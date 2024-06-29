6a848002bba80497d914d29a6bbbb8b3ab7d1e16
Nibras Sameeh <nibras.s@aeturnum.com>
2024-06-29 16:19:15 +0530

    code formatting

--- book-network-api/src/main/java/com/alibou/book/BookNetworkApiApplication.java
+++ book-network-api/src/main/java/com/alibou/book/BookNetworkApiApplication.java
@@ -25,9 +25,9 @@ public class BookNetworkApiApplication {
     @Bean
     public CommandLineRunner runner(RoleRepository roleRepository) {
         return args -> {
-          if (roleRepository.findByName("USER").isEmpty()) {
-              roleRepository.save(Role.builder().name("USER").build());
-          }
+            if (roleRepository.findByName("USER").isEmpty()) {
+                roleRepository.save(Role.builder().name("USER").build());
+            }
         };
     }
 
