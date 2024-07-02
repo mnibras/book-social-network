ad6f778c08bbfc04ff8865529cc1cc337c0205b2
Nibras Sameeh <nibras.s@aeturnum.com>
2024-07-02 13:53:10 +0530

    added ci and cd pipelines

--- book-network-api/src/main/java/com/alibou/book/config/BeansConfig.java
+++ book-network-api/src/main/java/com/alibou/book/config/BeansConfig.java
@@ -1,10 +1,10 @@
 package com.alibou.book.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -25,6 +25,9 @@ import java.util.List;
 public class BeansConfig {
 
     private final UserDetailsService userDetailsService;
+
+    @Value("${application.cors.origins:*}")
+    private List<String> allowedOrigin;
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
@@ -53,14 +56,11 @@ public class BeansConfig {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8080"));
-        config.setAllowedHeaders(Arrays.asList(
-                HttpHeaders.ORIGIN,
-                HttpHeaders.CONTENT_TYPE,
-                HttpHeaders.ACCEPT,
-                HttpHeaders.AUTHORIZATION
-        ));
+        // config.setAllowCredentials(true);
+        // config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8080"));
+        config.setAllowedOrigins(allowedOrigin);
+        config.setAllowedHeaders(List.of("*")); // not recommended for production
+        config.setAllowedMethods(List.of("*")); // not recommended for production
         config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
