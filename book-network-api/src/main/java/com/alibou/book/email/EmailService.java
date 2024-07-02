8cd69c986519eda98ce79bf313d4566f83f226dc
Nibras Sameeh <nibras.s@aeturnum.com>
2024-07-02 10:42:01 +0530

    replaced the mail server with gmail

--- book-network-api/src/main/java/com/alibou/book/email/EmailService.java
+++ book-network-api/src/main/java/com/alibou/book/email/EmailService.java
@@ -30,7 +30,7 @@ public class EmailService {
         if (emailTemplateName == null) {
             templateName = "confirm-email";
         } else {
-            templateName = emailTemplateName.name();
+            templateName = emailTemplateName.getName();
         }
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, MULTIPART_MODE_MIXED, UTF_8.name());
